7af30ca079226b7c7004c34686f309e4b51fa974
sr-nirmal <nirmalsr783@gmail.com>
2024-01-30 21:26:33 +0530

    first

--- rps/src/main/java/com/sap/rps/RpsServices.java
+++ rps/src/main/java/com/sap/rps/RpsServices.java
@@ -3,11 +3,56 @@ package com.sap.rps;
 import java.util.Random;
 
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class RpsServices{
 
     int rock = 0;
     int paper = 1;
     int scissor = 2;
+
+    public RpsServices(){
+        String connectionString = "mongodb+srv://deadshot:deadshot@cluster0.ptitmlu.mongodb.net/?retryWrites=true&w=majority";
+        
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                // Send a ping to confirm a successful connection
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        MongoDatabase database = mongoClient.getDatabase("database_01");
+        MongoCollection<Document> collection = database.getCollection("collection_03");
+
+        Document document = new Document("name", "John Doe").append("age", 30);
+        collection.insertOne(document);
+
+
+
+    }
     
     private int getRandom(){
         Random rand = new Random();
